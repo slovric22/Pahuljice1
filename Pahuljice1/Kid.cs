89ca4162fc8c19bc745b301d5205c720190ebf2a
Stjepan Lovrić <slovric22@foi.hr>
2025-05-08 12:26:54 +0200

    Klace Teacher i Kid za ravnateljjicu i djecu.

---
+++ Pahuljice1/Kid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pahuljice1
+{
+    public class Kid : Person
+    {
+        public int OIB { get; set; }
+    }
+}
